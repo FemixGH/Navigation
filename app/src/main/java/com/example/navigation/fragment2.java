a3bea44730339d0b572759442f054f8dec86595b
45161846 <1761556@gmail.com>
2022-04-28 00:09:35 +0300

    smth_new_2

--- app/src/main/java/com/example/navigation/fragment2.java
+++ app/src/main/java/com/example/navigation/fragment2.java
@@ -1,23 +1,70 @@
 package com.example.navigation;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class fragment2 extends Fragment {
+    Button search;
+    EditText edit;
+
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_fragment1,
+                container,false);
+
+        //search = view.findViewById(R.id.search_button_on_2);
+//        search.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(getActivity(), "OK", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+
+        edit=(EditText) view.findViewById(R.id.Search_text_on_2);
+
+
+        final InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+
+
+
+//            edit.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+//                @Override
+//                public void onFocusChange(View view, boolean hasFocus) {
+//                    if (!hasFocus) {
+//
+//                        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+//
+//                    }
+//                }
+//            });
+//
+
+
+
         return inflater.inflate(R.layout.fragment_fragment2, container, false);
     }
 
-//    int a;
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
 }
